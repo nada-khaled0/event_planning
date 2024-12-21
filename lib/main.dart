e4e47636fc84f4e71518acb4de8b930ab0060749
nada-khaled0 <yoondaaahh@gmail.com>
2024-12-21 23:53:22 +0200

    Initial project. Added empty home screen.

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:event_planning/home%20screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: HomeScreen.routeName,
+      routes: {
+        HomeScreen.routeName: (context) => HomeScreen(),
+      },
+    );
+  }
+}
